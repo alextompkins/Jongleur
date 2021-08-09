57890232eb3902c55c16b4261fc6243115bab4c2
Alex Tompkins <alex.tompkins@phocassoftware.com>
2021-08-09 16:26:59 +1200

    Add simple CameraPreview with blank ImageAnalyzer

---
+++ app/src/main/java/nz/atomic/jongleur/CameraPreview.kt
@@ -0,0 +1,58 @@
+package nz.atomic.jongleur
+
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+
+@Composable
+fun CameraPreview() {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val context = LocalContext.current
+    val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
+
+    AndroidView(
+        factory = { ctx ->
+            val previewView = PreviewView(ctx)
+            val executor = ContextCompat.getMainExecutor(ctx)
+            cameraProviderFuture.addListener({
+                val cameraProvider = cameraProviderFuture.get()
+                val preview = Preview.Builder().build().also {
+                    it.setSurfaceProvider(previewView.surfaceProvider)
+                }
+
+                val cameraSelector = CameraSelector.Builder()
+                    .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                    .build()
+
+                val analyzer = ImageAnalyzer()
+
+                val imageAnalysis = ImageAnalysis.Builder()
+                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                    .build()
+                    .apply {
+                        setAnalyzer(executor, analyzer)
+                    }
+
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    lifecycleOwner,
+                    cameraSelector,
+                    imageAnalysis,
+                    preview
+                )
+            }, executor)
+            previewView
+        },
+        modifier = Modifier.fillMaxSize()
+    )
+}
--- app/src/main/java/nz/atomic/jongleur/MainActivity.kt
+++ app/src/main/java/nz/atomic/jongleur/MainActivity.kt
@@ -1,16 +1,91 @@
 package nz.atomic.jongleur
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionRequired
+import com.google.accompanist.permissions.rememberPermissionState
 import nz.atomic.jongleur.ui.theme.JongleurTheme
+
+@Composable
+private fun Rationale(
+    onDoNotShowRationale: () -> Unit,
+    onRequestPermission: () -> Unit
+) {
+    Column {
+        Text("The camera is important for this app. Please grant the permission.")
+        Spacer(modifier = Modifier.height(8.dp))
+        Row {
+            Button(onClick = onRequestPermission) {
+                Text("Request permission")
+            }
+            Spacer(Modifier.width(8.dp))
+            Button(onClick = onDoNotShowRationale) {
+                Text("Don't show rationale again")
+            }
+        }
+    }
+}
+
+@Composable
+private fun PermissionDenied(
+    navigateToSettingsScreen: () -> Unit
+) {
+    Column {
+        Text(
+            "Camera permission denied. See this FAQ with information about why we " +
+                    "need this permission. Please, grant us access on the Settings screen."
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = navigateToSettingsScreen) {
+            Text("Open Settings")
+        }
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun Home(navigateToSettingsScreen: () -> Unit) {
+    val cameraPermissionState = rememberPermissionState(Manifest.permission.CAMERA)
+    var doNotShowRationale by rememberSaveable { mutableStateOf(false) }
+
+    PermissionRequired(
+        cameraPermissionState,
+        permissionNotGrantedContent = {
+            if (doNotShowRationale) {
+                Text("Feature not available")
+            } else {
+                Rationale(
+                    onDoNotShowRationale = { doNotShowRationale = true },
+                    onRequestPermission = { cameraPermissionState.launchPermissionRequest() }
+                )
+            }
+        },
+        permissionNotAvailableContent = {
+            PermissionDenied(navigateToSettingsScreen)
+        }
+    ) {
+        CameraPreview()
+    }
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,22 +94,26 @@ class MainActivity : ComponentActivity() {
             JongleurTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Home(
+                        navigateToSettingsScreen = {
+                            startActivity(
+                                Intent(
+                                    Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                                    Uri.fromParts("package", packageName, null)
+                                )
+                            )
+                        }
+                    )
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JongleurTheme {
-        Greeting("Android")
+        Home {}
     }
 }
